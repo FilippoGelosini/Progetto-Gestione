19a64822b81f8a2706f7416f8a79156b29438716
Filippo Gelosini <filippo.gelosini02@gmail.com>
2024-05-19 12:29:28 +0200

    first commit

---
+++ indexer.py
@@ -0,0 +1,82 @@
+from whoosh.fields import Schema, TEXT, DATETIME, NUMERIC, BOOLEAN
+from whoosh.index import create_in, open_dir
+from datetime import datetime
+import csv
+import os
+import pickle
+
+dataset_path = "steam_reviews_small.csv"
+words_path = "title_words.pickle"
+main_path = "index"
+title_path = "title"
+main_index = None
+title_index = None
+limit = 10
+title_list = set()
+title_words = set()
+
+main_schema = Schema(
+    date = DATETIME(stored=True),
+    hours_played = NUMERIC(stored=True),
+    is_early_access = BOOLEAN(stored=True),
+    recommendation = TEXT(stored=True),
+    review = TEXT(stored=True),
+    title = TEXT(stored=True),
+)
+
+title_schema = Schema(
+    title = TEXT(stored=True)
+)
+
+def get_index(dir, schema_type, index_type):
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+        ix = create_in(dir, schema_type)
+        print(f"{index_type} index created")
+    else:
+        ix = open_dir(dir)
+        print(f"{index_type} index retrieved")
+    return ix
+
+def add_to_index(main_index, title_index):
+    with open(dataset_path, "r", encoding="utf-8") as file:
+        csvFile = csv.reader(file)
+        next(csvFile)
+        main_writer = main_index.writer()
+        title_writer = title_index.writer()
+        counter = 1
+        for row in csvFile:
+            try:
+                date = datetime.strptime(row[0], "%Y-%m-%d")
+                playtime = int(row[3])
+                early_access = bool(row[4])
+                recommendation = row[5]
+                review = row[6]
+                title = row[7]
+
+                
+                main_writer.add_document(date=date, hours_played=playtime, is_early_access=early_access, recommendation=recommendation, review=review, title=title)
+                print(f"Review added to the index - {counter}")
+                counter += 1
+
+                if title not in title_list:
+                    title_writer.add_document(title=title)
+
+                title_list.add(title)
+
+                for term in title.lower().split():
+                    title_words.add(term.strip(".,:;/\\\"!?'^£$%&()€*+-#@<>_®™"))
+            except Exception as e:
+                print(f"Error adding document: {e}")
+                continue
+        main_writer.commit()
+        title_writer.commit()
+
+        title_words.remove('')
+        with open(words_path, "wb") as file:
+            pickle.dump(title_words, file)
+
+if __name__ == "__main__":
+    main_index = get_index(main_path, main_schema, "Main")
+    title_index = get_index(title_path, title_schema, "Title")
+    add_to_index(main_index, title_index)
---
+++ searcher.py
@@ -0,0 +1,81 @@
+from whoosh.index import open_dir
+from whoosh.qparser import SimpleParser, MultifieldParser
+from whoosh import scoring 
+import os
+import pickle
+
+words_path = "title_words.pickle"
+main_path = "index"
+title_path = "title"
+fieldlist = ["date", "hours_played", "is_early_access", "recommendation", "review", "title"]
+main_index = None
+title_index = None
+limit = 10
+
+file = open(words_path, "rb")
+title_words = pickle.load(file)
+file.close()
+
+def get_index(dir, index_type):
+    if not os.path.exists(dir):
+        print(f"{index_type} index not found. Exiting")
+        exit()
+    else:
+        ix = open_dir(dir)
+        print(f"{index_type} index retrieved")
+    return ix
+
+def print_hit_result(hit, reviews):
+    if reviews:
+        print(f"Review Date: {hit.get('date')}")
+        print(f"Playtime: {hit.get('hours_played')} hours")
+        print(f"Early Access: {hit.get('is_early_access')}")
+        print(f"Recommendation: {hit.get('recommendation')}")
+        print(f"Review: {hit.get('review')}")
+    
+    print(f"Title: {hit.get('title')}")
+    print("\n")
+
+def select_index(query):
+    tokens = query.lower().split()
+    reviews = False
+
+    for token in tokens:
+        if token.strip(".,:;/\\\"!?'^£$%&()€*+-#@<>_®™") not in title_words:
+            reviews = True
+
+    return reviews
+
+def full_text_search(main_index, title_index):
+    while True: 
+        raw_query = input("Insert query, press enter to stop: ")
+
+        if not raw_query.strip():
+            break
+
+        reviews = select_index(raw_query)
+
+        if reviews:
+            parser = MultifieldParser(fieldlist, schema=main_index.schema)
+            searcher = main_index.searcher(weighting=scoring.BM25F()) 
+        else:
+            parser = SimpleParser("title", schema=title_index.schema)
+            searcher = title_index.searcher()
+        
+        query = parser.parse(raw_query)
+        results = searcher.search(query, limit=limit, scored=True)
+
+        i = 1
+        if results:
+            print(f"Results for '{raw_query}': " + "\n")
+            for hit in results:
+                print(f"{i})")
+                i += 1
+                print_hit_result(hit, reviews)
+        else:
+            print(f"No results found for query \"{raw_query}\"")
+
+if __name__ == "__main__":
+    main_index = get_index(main_path, "Main")
+    title_index = get_index(title_path, "Title")
+    full_text_search(main_index, title_index)
